20bda6b4da763faf7faad36d09dba74b5e37e2f9
123ABCDF11345 <50321657+123ABCDF11345@users.noreply.github.com>
2019-05-14 11:10:44 +0800

    【重要】更新教师端1.3.0
    教师端1.3.0更新日志：
    重构整体框架
    对读取分数模块进行优化，适配学生端
    增加自动报错功能
    增加设置人数选项
    优化密码输入界面
    修复了一些bug

--- 选择题自动批改系统-教师端.py
+++ 选择题自动批改系统-教师端.py
@@ -1,7 +1,8 @@
 #coding=utf-8
+import json
 def jiaoshikongzhitai():
-	shurutishi="按1进入题目修改，按2进入答案修改，按3显示分数，"
-	shurutishi2="按4改密码，按5查看关于作者的信息：   "
+	shurutishi="按1进入题目修改（必设），按2进入答案修改，按3显示分数，"
+	shurutishi2="按4改密码，按5查看关于作者的信息，按6设置考试人数（必设）：   "
 	kongzhitaijianru=input(shurutishi+shurutishi2)
 	try:
 		kongzhitaijianru=int(kongzhitaijianru)
@@ -53,7 +54,7 @@ def jiaoshikongzhitai():
 			filename="daan5.txt"
 			with open(filename,"w")as file_object:
 				file_object.write(daan5)
-			print("所有答案已经录入完毕，可以进行考试。考试方法：关闭教师端，启动学生端 ")
+			print("所有答案已经录入完毕，现在不可以进行考试！请录入人数！")
 			print("注意：不得删除同文件夹下的任何txt文件")
 			print("作者友情提醒您：考试并不能反映一个学生的成绩水平，请勿擅作主张")
 			print("即将返回主菜单")
@@ -61,11 +62,13 @@ def jiaoshikongzhitai():
 		elif kongzhitaijianru==3:
 			print("先行说明：本功能只有在已经进行过至少一次学生端考试时可以使用！")
 			print("否则可能报错或者没有输出")
-			filename='chengji.txt'
+			filename='chengji.json'
 			try:
 				with open(filename)as file_object:
-					for line in file_object:
-						print(line)
+					chengjizidian=file_object.read()
+					for cg,xm in chengjizidian.items():
+						print("\n姓名："+xm)
+						print("\n成绩："+cg)
 			except FileNotFoundError:
 				print("   ")
 				print("   ")
@@ -96,6 +99,14 @@ def jiaoshikongzhitai():
 			print("作者联系方式：邮箱2542594900@qq.com  GitHub：123ABCDF11345")
 			print("即将返回主菜单")
 			jiaoshikongzhitai()
+		elif kongzhitaijianru==6:
+			jianrurenshu=input("请输入本次考试的人数： ")
+			
+			filename="renshu.txt"
+			with open(filename,"w")as file_object:
+				file_object.write(jianrurenshu)
+			print("输入完成，即将返回主菜单。可以考试，考试方法：关闭教师端，启动学生端 ")
+			jiaoshikongzhitai()
 		else:
 			print("无效的指令")
 			print("©版权所有，侵版必究  2019 汪俊择 123ABCDF11345")
@@ -112,10 +123,10 @@ try:
 except FileNotFoundError:
 	print("   ")
 	print("   ")
-	print("没有找到基本文件，请确认创建了一个叫“mima.txt”的空记事本文件 强烈建议您使用GitHub上下载的文件，自动提供基本文件！ ")
+	print("没有找到基本文件，请确认创建了一个叫“mima.txt”的空记事本文件，并将文件里写入0000， 强烈建议您使用GitHub上下载的文件，自动提供基本文件！ ")
 	print("程序结束，请在建立基本文件后再启动")
 else:
-	print("给从GitHub下载的用户：初始密码为0000")
+	print("初始密码为0000")
 	jianrumima=input("请输入密码，密码长度1-8位，无密码回车进入系统  不知道密码？直接回车试试：   ")
 	try:
 		jianrumima=int(jianrumima)
@@ -132,3 +143,5 @@ else:
 			else:
 				print("您输入的密码错误！")  
 #程序结束  ©版权所有，侵版必究  2019 汪俊择 123ABCDF11345
+
+
